b24601bb7ab628a4f7afeb5dc470d1621039c16e
Enow Divine <sirdivine16@gmail.com>
2023-12-16 01:38:41 +0100

    contrat update

--- src/utils/constants.js
+++ src/utils/constants.js
@@ -1,4 +1,4 @@
 import abi from "./Transactions.json";
 
-export const contractAddress = "0xfCCF80344a668b72ac4Be23513F0E9E4a35C84fA";
+export const contractAddress = "0xb7Bb920144C3602F91BE56668964866C97297cfe";
 export const contractABI = abi.abi;
